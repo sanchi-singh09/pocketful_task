b373aab17be27c45ddd07b63ead8f834c95b7780
Sanchi Singh <singhsanchi09@gmail.com>
2025-01-07 00:18:09 +0530

    change in experience

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "./Experiences.module.css";
 import Grid from "@mui/material/Grid";
 import Image from "next/image";
-import companyLogo from "./kiet.jpeg";
+import companyLogo1 from "./kiet.jpeg";
+import companyLogo2 from "./bcalogo.jpeg";
+import companyLogo3 from "./rhpslogo.jpeg";
 
 const Experiences = () => {
   return (
@@ -13,7 +15,7 @@ const Experiences = () => {
           <Grid item xs={6}>
             <div className={styles.imageContainer}>
               <div>
-                <Image src={companyLogo} alt="Company Logo" width={200} />
+                <Image src={companyLogo1} alt="Company Logo1" width={200} />
               </div>
 
               <div className={styles.text}>Aspiring Software Developer</div>
@@ -23,21 +25,21 @@ const Experiences = () => {
           <Grid item xs={6}>
             <div className={styles.imageContainer}>
               <div>
-                <Image src={companyLogo} alt="Company Logo" width={200} />
+                <Image src={companyLogo2} alt="Company Logo2" width={200} />
               </div>
 
               <div className={styles.text}>Aspiring Software Developer</div>
-              <div className={styles.text}>Aug 2023 – May 2025</div>
+              <div className={styles.text}>Aug 2020 – May 2023</div>
             </div>
           </Grid>
           <Grid item xs={6}>
             <div className={styles.imageContainer}>
               <div>
-                <Image src={companyLogo} alt="Company Logo" width={200} />
+                <Image src={companyLogo3} alt="Company Logo3" width={200} />
               </div>
 
               <div className={styles.text}>Aspiring Software Developer</div>
-              <div className={styles.text}>Aug 2023 – May 2025</div>
+              <div className={styles.text}>Aug 2018 – May 2020</div>
             </div>
           </Grid>
         </Grid>
